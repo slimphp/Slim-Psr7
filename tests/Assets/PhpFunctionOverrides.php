19b22f3a8f13a9551313ddda5e83df1eaec3ddda
Marcel Hernandez <e6990620@gmail.com>
2019-05-01 00:30:20 +0200

    tests for NonBufferedBody mocking header() and header_remove()

--- tests/Assets/PhpFunctionOverrides.php
+++ tests/Assets/PhpFunctionOverrides.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 
 namespace Slim\Psr7;
 
+use Slim\Tests\Psr7\Assets\HeaderStack;
+
 /**
  * Return the value of the global variable $GLOBALS['getallheaders_return'] if it exists. Otherwise the
  * function override calls the default php built-in function.
@@ -22,4 +24,32 @@ function getallheaders()
     }
 
     return \getallheaders();
+}
+
+/**
+ * Emit a header, without creating actual output artifacts
+ *
+ * @param string   $string
+ * @param bool     $replace
+ * @param int|null $statusCode
+ */
+function header($string, $replace = true, $statusCode = null)
+{
+    HeaderStack::push(
+        [
+            'header'      => $string,
+            'replace'     => $replace,
+            'status_code' => $statusCode,
+        ]
+    );
+}
+
+/**
+ * Remove a previously emmited header from the HeaderStack.
+ *
+ * @param string|null $name
+ */
+function header_remove($name = null)
+{
+    HeaderStack::remove($name);
 }
