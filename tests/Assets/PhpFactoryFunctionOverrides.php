0a6125784dbda7b6f07b41ea6aa3e1bac20b7c5e
l0gicgate <pierre@lgse.com>
2019-05-03 12:07:44 -0600

    add tests for all StreamFactory methods

--- tests/Assets/PhpFactoryFunctionOverrides.php
+++ tests/Assets/PhpFactoryFunctionOverrides.php
@@ -25,3 +25,20 @@ function is_readable(string $filename): bool
 
     return \is_readable($filename);
 }
+
+/**
+ * Return global set value if set or value from php built-in function.
+ *
+ * @param $filename
+ * @param $mode
+ *
+ * @return bool|resource
+ */
+function fopen($filename, $mode)
+{
+    if (isset($GLOBALS['fopen_return'])) {
+        return isset($GLOBALS['fopen_return']);
+    }
+
+    return \fopen($filename, $mode);
+}
