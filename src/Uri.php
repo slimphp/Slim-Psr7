b6de109e7fbea188da9a1c66cf920ba05e732774
Luke Kuzmish <lakuzmish@gmail.com>
2023-11-23 08:30:58 -0500

    allow filterPort as string

--- src/Uri.php
+++ src/Uri.php
@@ -300,7 +300,13 @@ class Uri implements UriInterface
      */
     protected function filterPort($port): ?int
     {
-        if (is_null($port) || (is_integer($port) && ($port >= 1 && $port <= 65535))) {
+        if (is_null($port)) {
+            return null;
+        }
+
+        $port = (int) $port;
+
+        if ($port >= 1 && $port <= 65535) {
             return $port;
         }
 
