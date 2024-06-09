382e260116362be02e67e91ed18c55030a3e49be
William Desportes <williamdes@wdes.fr>
2024-06-09 13:18:13 +0200

    Use a new class TestObject

--- tests/UriTest.php
+++ tests/UriTest.php
@@ -15,15 +15,6 @@ use PHPUnit\Framework\TestCase;
 use Slim\Psr7\Uri;
 use stdClass;
 
-final class TestObject implements \Stringable {
-
-    public function __construct(private readonly string $value) {}
-
-    public function __toString(): string {
-        return $this->value;
-    }
-}
-
 class UriTest extends TestCase
 {
     public function uriFactory(): Uri
